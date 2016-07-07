e5e7ff550706c09021acfa2387aec25e4b85b5f7
Dave Moten <davidmoten@gmail.com>
2016-07-07 16:48:49 +1000

    dev

--- tv-recorder-ui/src/main/java/com/github/davidmoten/tv/SearchServlet.java
+++ tv-recorder-ui/src/main/java/com/github/davidmoten/tv/SearchServlet.java
@@ -16,109 +16,89 @@ import com.github.davidmoten.xmltv.Title;
 
 public class SearchServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -4914856137989997740L;
-    private final TvGuide guide;
+	private static final long serialVersionUID = -4914856137989997740L;
+	private final TvGuide guide;
 
-    public SearchServlet(TvGuide tvGuide) {
-        this.guide = tvGuide;
-    }
+	public SearchServlet(TvGuide tvGuide) {
+		this.guide = tvGuide;
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        String search = req.getParameter("q");
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println(
-                "<html><head><link rel=\"stylesheet\" href=\"css/style.css\"/><title>Results</title></head><body>");
-        guide.search(search).sorted((a, b) -> a.getStart().compareTo(b.getStart()))
-                .map(p -> toHtml(p)).forEach(s -> out.println(s));
-        out.println("</body></html>");
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String search = req.getParameter("q");
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html><head><link rel=\"stylesheet\" href=\"css/style.css\"/><title>Results</title></head><body>");
+		guide.search(search).sorted((a, b) -> a.getStart().compareTo(b.getStart())).map(p -> toHtml(p))
+				.forEach(s -> out.println(s));
+		out.println("</body></html>");
 
-    }
+	}
 
-    private String toHtml(Programme p) {
-        StringBuilder s = new StringBuilder();
-        s.append("<div class='time'>");
-        s.append(formatTime(p.getStart()));
-        s.append("</div>");
-        s.append("<div class='channel'>");
-        s.append(" ");
-        s.append(p.getChannel());
-        s.append("</div>");
-        s.append(" ");
-        for (Title t : p.getTitle()) {
-            s.append("<div class='title'>");
-            s.append(t.getvalue());
-            s.append("</div>");
-        }
-        s.append(" ");
-        for (SubTitle t : p.getSubTitle()) {
-            s.append("<div class='subTitle'>");
-            s.append(t.getvalue());
-            s.append("</div>");
-        }
-        s.append("</div>");
-        s.append("<div class='afterHeader'></div>");
+	private String toHtml(Programme p) {
+		StringBuilder s = new StringBuilder();
+		s.append("<div class='time'>");
+		s.append(formatTime(p.getStart()));
+		s.append("</div>");
+		s.append("<div class='channel'>");
+		s.append(" ");
+		s.append(p.getChannel());
+		s.append("</div>");
+		s.append(" ");
+		for (Title t : p.getTitle()) {
+			s.append("<div class='title'>");
+			s.append(t.getvalue());
+			s.append("</div>");
+		}
+		s.append(" ");
+		for (SubTitle t : p.getSubTitle()) {
+			s.append("<div class='subTitle'>");
+			s.append(t.getvalue());
+			s.append("</div>");
+		}
+		s.append("</div>");
+		s.append("<div class='afterHeader'></div>");
 
-        for (Desc d : p.getDesc()) {
-            if (!d.getvalue().contains("Please donate")) {
-                s.append("<div class='description'>");
-                s.append(d.getvalue());
-                s.append("</div>");
-            }
-        }
-        if (!p.getCategory().isEmpty()) {
-            s.append("<div class='label'>");
-            s.append("Categories: ");
-            s.append("</div>");
-            s.append("<div class='detail'>");
-            s.append(p.getCategory().stream().map(c -> c.getvalue())
-                    .collect(Collectors.joining(", ")));
-            s.append("</div>");
-        }
-        if (!p.getKeyword().isEmpty()) {
-            s.append("<div class='label'>");
-            s.append("Keywords: ");
-            s.append("</div>");
-            s.append("<div class='detail'>");
-            s.append(p.getKeyword().stream().map(c -> c.getvalue())
-                    .collect(Collectors.joining(", ")));
-            s.append("</div>");
-        }
-        if (!p.getRating().isEmpty()) {
-            s.append("<div class='label'>");
-            s.append("Ratings: ");
-            s.append("</div>");
-            s.append("<div class='detail'>");
-            s.append(p.getRating().stream().map(c -> c.getValue())
-                    .collect(Collectors.joining(", ")));
-            s.append("</div>");
-        }
-        if (!p.getCountry().isEmpty()) {
-            s.append("<div class='label'>");
-            s.append("Country: ");
-            s.append("</div>");
-            s.append("<div class='detail'>");
-            s.append(p.getCountry().stream().map(c -> c.getvalue())
-                    .collect(Collectors.joining(", ")));
-            s.append("</div>");
-        }
-        if (p.getCredits() != null) {
-            s.append("<div class='label'>");
-            s.append("Actors: ");
-            s.append("</div>");
-            s.append("<div class='detail'>");
-            s.append(p.getCredits().getActor().stream().map(c -> c.getvalue())
-                    .collect(Collectors.joining(", ")));
-            s.append("</div>");
-        }
-        return s.toString();
-    }
+		for (Desc d : p.getDesc()) {
+			if (!d.getvalue().contains("Please donate")) {
+				s.append("<div class='description'>");
+				s.append(d.getvalue());
+				String rating = p.getRating().stream().map(c -> c.getValue()).collect(Collectors.joining(", "));
+				if (rating.length()>0){
+					s.append(" [" + rating + "]");
+				}
+				s.append("</div>");
+			}
+		}
+		if (!p.getCategory().isEmpty()) {
+			label(s, "Category", p.getCategory().stream().map(c -> c.getvalue()).collect(Collectors.joining(", ")));
+		}
+		if (!p.getKeyword().isEmpty()) {
+			label(s, "Keywords ", p.getKeyword().stream().map(c -> c.getvalue()).collect(Collectors.joining(", ")));
+		}
+		if (!p.getCountry().isEmpty()) {
+			label(s, "Country", p.getCountry().stream().map(c -> c.getvalue()).collect(Collectors.joining(", ")));
+		}
+		if (p.getCredits() != null) {
+			label(s, "Actors",
+					p.getCredits().getActor().stream().map(c -> c.getvalue()).collect(Collectors.joining(", ")));
+		}
+		return s.toString();
+	}
 
-    private Object formatTime(String t) {
-        return t.substring(0, 4) + "/" + t.substring(4, 6) + "/" + t.substring(6, 8) + " "
-                + t.substring(8, 10) + ":" + t.substring(10, 12);
-    }
+	private static void label(StringBuilder s, String label, String value) {
+		s.append("<div class='label'>");
+		s.append(label + ":");
+		s.append("</div>");
+		s.append("<div class='detail'>");
+		s.append(value);
+		s.append("</div>");
+		s.append("<div class='afterDetail'></div>");
+	}
+
+	private Object formatTime(String t) {
+		return t.substring(0, 4) + "/" + t.substring(4, 6) + "/" + t.substring(6, 8) + " " + t.substring(8, 10) + ":"
+				+ t.substring(10, 12);
+	}
 
 }
