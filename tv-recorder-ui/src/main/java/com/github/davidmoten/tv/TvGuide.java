ca1810909f790d90f2be7fa4e5fecea5eed2a19a
David Moten <davidmoten@gmail.com>
2016-07-09 21:50:14 +1000

    dev

--- tv-recorder-ui/src/main/java/com/github/davidmoten/tv/TvGuide.java
+++ tv-recorder-ui/src/main/java/com/github/davidmoten/tv/TvGuide.java
@@ -1,5 +1,10 @@
 package com.github.davidmoten.tv;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Stream;
 
 import javax.xml.bind.JAXBContext;
@@ -19,64 +24,68 @@ import com.github.davidmoten.xmltv.Tv;
 
 public class TvGuide {
 
-    public Tv get() {
-        return tv();
-    }
+	public Tv get() {
+		return tv();
+	}
 
-    private static Tv tv() {
-        try {
-            final JAXBContext context = JAXBContext.newInstance(Tv.class);
-            final Unmarshaller u = context.createUnmarshaller();
-            return u.unmarshal(new StreamSource(TvGuide.class.getResourceAsStream("/sample.xml")),
-                    Tv.class).getValue();
-        } catch (JAXBException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private static Tv tv() {
+		try {
+			final JAXBContext context = JAXBContext.newInstance(Tv.class);
+			final Unmarshaller u = context.createUnmarshaller();
+			return u.unmarshal(new StreamSource(TvGuide.class.getResourceAsStream("/sample.xml")), Tv.class).getValue();
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public Stream<Programme> search(String search) {
-        return get().getProgramme().stream() //
-                .filter(p -> text(p).contains(search.toUpperCase()));
-    }
+	public Stream<Programme> search(String search) {
+		return get().getProgramme().stream() //
+				.filter(p -> text(p).contains(search.toUpperCase())) //
+				.filter(p -> {
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+					LocalDateTime dateTime = LocalDateTime.parse(p.getStop().substring(0, 12), formatter);
+					return dateTime.isAfter(LocalDateTime.now());
+				});
+	}
 
-    private static String text(Programme p) {
-        StringBuilder s = new StringBuilder();
-        add(s, p.getChannel());
-        add(s, p.getClumpidx());
-        for (Title t : p.getTitle()) {
-            add(s, t.getvalue());
-        }
-        for (SubTitle t : p.getSubTitle()) {
-            add(s, t.getvalue());
-        }
-        for (Category t : p.getCategory()) {
-            add(s, t.getvalue());
-        }
-        for (Country t : p.getCountry()) {
-            add(s, t.getvalue());
-        }
-        for (Desc t : p.getDesc()) {
-            add(s, t.getvalue());
-        }
-        for (Keyword t : p.getKeyword()) {
-            add(s, t.getvalue());
-        }
-        if (p.getLanguage() != null) {
-            add(s, p.getLanguage().getvalue());
-        }
-        if (p.getCredits() != null)
-            for (Actor t : p.getCredits().getActor()) {
-                add(s, t.getvalue());
-                add(s, t.getRole());
-            }
-        return s.toString().toUpperCase();
-    }
+	private static String text(Programme p) {
+		StringBuilder s = new StringBuilder();
+		add(s, p.getChannel());
+		add(s, p.getClumpidx());
+		for (Title t : p.getTitle()) {
+			add(s, t.getvalue());
+		}
+		for (SubTitle t : p.getSubTitle()) {
+			add(s, t.getvalue());
+		}
+		for (Category t : p.getCategory()) {
+			add(s, t.getvalue());
+		}
+		for (Country t : p.getCountry()) {
+			add(s, t.getvalue());
+		}
+		for (Desc t : p.getDesc()) {
+			add(s, t.getvalue());
+		}
+		for (Keyword t : p.getKeyword()) {
+			add(s, t.getvalue());
+		}
+		if (p.getLanguage() != null) {
+			add(s, p.getLanguage().getvalue());
+		}
+		if (p.getCredits() != null)
+			for (Actor t : p.getCredits().getActor()) {
+				add(s, t.getvalue());
+				add(s, t.getRole());
+			}
+		return s.toString().toUpperCase();
+	}
 
-    private static void add(StringBuilder s, String string) {
-        if (string != null && string.trim().length() > 0) {
-            s.append(string);
-            s.append(' ');
-        }
-    }
+	private static void add(StringBuilder s, String string) {
+		if (string != null && string.trim().length() > 0) {
+			s.append(string);
+			s.append(' ');
+		}
+	}
 
 }
